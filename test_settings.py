1c0e2005066dbcb4842a0e86f341512aa687fd32
Iacopo Spalletti <i.spalletti@nephila.it>
2014-12-28 00:12:36 +0100

    Add example settings to run a project with djangocms-helper

---
+++ test_settings.py
@@ -0,0 +1,17 @@
+HELPER_SETTINGS = {
+    'TIME_ZONE': 'Europe/Zurich',
+    'LANGUAGES': (
+        ('en', 'English'),
+        ('de', 'German'),
+    ),
+    'INSTALLED_APPS': [
+        'aldryn_newsblog',
+        'aldryn_people',
+        'easy_thumbnails',
+        'parler',
+        'djangocms_text_ckeditor',
+        'hvad',
+        'filer',
+        'cms.test_utils.project.sampleapp'
+    ],
+}
