6db168b93aba248ed1eb1afe3882a75e5b46efef
Martin Koistinen <mkoistinen@gmail.com>
2015-03-14 18:25:47 -0400

    Beef-up the templatetag: namespace_url
    It can now accept a kwarg: namespace, this allows this tag to also work on non-apphook'ed views or plugins, or simply, for overriding the namespace to use on apphook'ed views.

--- aldryn_apphooks_config/templatetags/apphooks_config_tags.py
+++ aldryn_apphooks_config/templatetags/apphooks_config_tags.py
@@ -12,18 +12,31 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def namespace_url(context, view_name, *args, **kwargs):
     """
-    Returns an absolute URL matching given view with its parameters.
+    Returns an absolute URL matching named view with its parameters and the
+    provided application instance namespace. If no namespace is passed as a
+    kwarg (or it is "" or None), this templatetag will look into the request
+    object for the app_config's namespace. If there is still no namespace found,
+    this tag will act like the normal {% url ... %} tag.
     """
 
-    namespace, config = get_app_instance(context['request'])
+    namespace = kwargs.pop('namespace', None)
+
+    if not namespace:
+        namespace, __ = get_app_instance(context['request'])
+
+    if namespace:
+        namespace += ":"
+
+    print("Using namespace [{0}]".format(namespace))
+
     if kwargs:
         return urlresolvers.reverse(
-            '{0:s}:{1:s}'.format(config.namespace, view_name),
+            '{0:s}{1:s}'.format(namespace, view_name),
             kwargs=kwargs)
     elif args:
         return urlresolvers.reverse(
-            '{0:s}:{1:s}'.format(config.namespace, view_name),
+            '{0:s}{1:s}'.format(namespace, view_name),
             args=args)
     else:
         return urlresolvers.reverse(
-            '{0:s}:{1:s}'.format(config.namespace, view_name))
+            '{0:s}{1:s}'.format(namespace, view_name))
