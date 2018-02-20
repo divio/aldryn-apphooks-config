bc0f64ea39ea836ed8b39c497631780e7a9e1a49
Robert Stein <robert@blueshoe.de>
2018-02-20 11:26:33 +0100

    updates widget rendering for Django >=1.11

--- aldryn_apphooks_config/widgets.py
+++ aldryn_apphooks_config/widgets.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+from cms.utils.compat import DJANGO_1_10
 from django import forms
 from django.utils.html import mark_safe
 
 
 class AppHookConfigWidget(forms.Select):
+    template_name = 'aldryn_apphooks_config/admin/apphook_config_widget.html'
 
     class Media:
         js = ('js/aldryn_apphooks_config/aldryn_apphooks_config.js',)
@@ -15,25 +17,24 @@ class AppHookConfigWidget(forms.Select):
             out = super(AppHookConfigWidget, self).render(name, value, attrs, choices)
         else:
             out = super(AppHookConfigWidget, self).render(name, value, attrs)
-        try:
+        if not DJANGO_1_10:
+            return out
+        else:
             final_attrs = self.build_attrs(attrs, name=name)
-        except TypeError:
-            attrs['name'] = name
-            final_attrs = self.build_attrs(attrs)
-        script = """
-        <script>
-        (function($) {
-            $(document).ready(function () {
-                var sel = $('#%(id)s');
-                if (!sel.val()) {
-                    sel.change(function () {
-                        $(this).apphook_reload_admin(
-                            '%(name)s', $("option:selected",$(this)).val()
-                        );
-                    });
-                }
-            });
-        })(django.jQuery);
-        </script>
-""" % final_attrs
-        return mark_safe(script + out)
+            script = """
+            <script>
+            (function($) {
+                $(document).ready(function () {
+                    var sel = $('#%(id)s');
+                    if (!sel.val()) {
+                        sel.change(function () {
+                            $(this).apphook_reload_admin(
+                                '%(name)s', $("option:selected",$(this)).val()
+                            );
+                        });
+                    }
+                });
+            })(django.jQuery);
+            </script>
+    """ % final_attrs
+            return mark_safe(script + out)
