0d9454c72bf844a15a5028ee427ce03d8dbd8824
Iacopo Spalletti <i.spalletti@nephila.it>
2018-03-10 22:09:27 +0100

    Reuse the same widget template in Django 1.10 and below

--- aldryn_apphooks_config/widgets.py
+++ aldryn_apphooks_config/widgets.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.utils.compat import DJANGO_1_10
 from django import forms
+from django.template.loader import render_to_string
 from django.utils.html import mark_safe
 
 
@@ -21,20 +22,6 @@ class AppHookConfigWidget(forms.Select):
             return out
         else:
             final_attrs = self.build_attrs(attrs, name=name)
-            script = """
-            <script>
-            (function($) {
-                $(document).ready(function () {
-                    var sel = $('#%(id)s');
-                    if (!sel.val()) {
-                        sel.change(function () {
-                            $(this).apphook_reload_admin(
-                                '%(name)s', $("option:selected",$(this)).val()
-                            );
-                        });
-                    }
-                });
-            })(django.jQuery);
-            </script>
-    """ % final_attrs
+            final_attrs['DJANGO_110'] = True
+            script = render_to_string(self.template_name, context=final_attrs)
             return mark_safe(script + out)
