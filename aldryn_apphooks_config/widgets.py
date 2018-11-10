divio/aldryn-apphooks-config

commit 795e39d48c87c782ad8becb883773f209a7887f8

Django 1.11 compatibility (#54)
* Add Django 1.11 compatibility
* Add django CMS 3.5 compatibility
* Implement django-appdata 0.2 interface
* Remove south migrations
* Drop support for django CMS 3.3 and below

aldryn_apphooks_config/widgets.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+from cms.utils.compat import DJANGO_1_10
 from django import forms
+from django.template.loader import render_to_string
 from django.utils.html import mark_safe
 
 
 class AppHookConfigWidget(forms.Select):
+    template_name = 'aldryn_apphooks_config/admin/apphook_config_widget.html'
 
     class Media:
         js = ('js/aldryn_apphooks_config/aldryn_apphooks_config.js',)
@@ -15,21 +18,10 @@ class AppHookConfigWidget(forms.Select):
             out = super(AppHookConfigWidget, self).render(name, value, attrs, choices)
         else:
             out = super(AppHookConfigWidget, self).render(name, value, attrs)
-        final_attrs = self.build_attrs(attrs, name=name)
-        script = """
-        <script>
-        (function($) {
-            $(document).ready(function () {
-                var sel = $('#%(id)s');
-                if (!sel.val()) {
-                    sel.change(function () {
-                        $(this).apphook_reload_admin(
-                            '%(name)s', $("option:selected",$(this)).val()
-                        );
-                    });
-                }
-            });
-        })(django.jQuery);
-        </script>
-""" % final_attrs
-        return mark_safe(script + out)
+        if not DJANGO_1_10:
+            return out
+        else:
+            final_attrs = self.build_attrs(attrs, name=name)
+            final_attrs['DJANGO_110'] = True
+            script = render_to_string(self.template_name, context=final_attrs)
+            return mark_safe(script + out)

aldryn_apphooks_config/tests/test_config.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 
 from cms import api
 from cms.apphook_pool import apphook_pool
-from cms.utils import get_cms_setting
+from cms.utils.conf import get_cms_setting
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import SimpleCookie
@@ -28,6 +28,10 @@ class AppHookConfigTestCase(BaseTestCase):
         self.language = settings.LANGUAGES[0][0]
         self.root_page = api.create_page(
             'root page', self.template, self.language, published=True)
+        # This is needed in django CMS 3.5+ to keep the same tree across
+        # all django CMS versions
+        if hasattr(self.root_page, 'set_as_homepage'):
+            self.root_page.set_as_homepage()
 
         self.ns_app_1 = ExampleConfig.objects.create(namespace='app1')
         self.ns_app_1.app_data.config.property = 'app1_property'
@@ -61,6 +65,7 @@ class AppHookConfigTestCase(BaseTestCase):
             for language, _ in settings.LANGUAGES[1:]:
                 api.create_title(language, page.get_slug(), page)
                 page.publish(language)
+        self.reload_urlconf()
 
     def test_configs(self):
         app = apphook_pool.get_apphook(self.page_1.application_urls)
@@ -336,9 +341,20 @@ class AppHookConfigTestCase(BaseTestCase):
 
         # Testing admin output for sample app specific implementation
         response = admin_instance.change_view(request, str(self.ns_app_1.pk))
-        self.assertContains(response, '<p>aldryn_apphooks_config.tests.utils.example.cms_appconfig.ExampleConfig</p>')
-        self.assertContains(response, '<p>app1</p>')
-        self.assertContains(response, 'name="config-property" type="text" value="app1_property"')
+        try:
+            self.assertContains(
+                response,
+                '<div class="readonly">aldryn_apphooks_config.tests.utils.example.cms_appconfig.ExampleConfig</div>'
+            )
+            self.assertContains(response, '<div class="readonly">app1</div>')
+            self.assertContains(response, 'value="app1_property"')
+        except AssertionError:
+            self.assertContains(
+                response,
+                '<p>aldryn_apphooks_config.tests.utils.example.cms_appconfig.ExampleConfig</p>'
+            )
+            self.assertContains(response, '<p>app1</p>')
+            self.assertContains(response, 'name="config-property" type="text" value="app1_property"')
 
     def test_admin(self):
         from django.contrib import admin
@@ -361,13 +377,22 @@ class AppHookConfigTestCase(BaseTestCase):
         response = admin_instance.add_view(request)
         self.assertContains(response, '$(this).apphook_reload_admin')
         self.assertContains(response, 'aldryn_apphooks_config')
-        self.assertContains(response, '<option value="1" selected="selected">%s</option>' % self.ns_app_1)
-        self.assertContains(response, '<input id="id_published"')
+        self.assertRegexpMatches(
+            force_text(response.content),
+            '(<option value="1" selected="selected">%s</option>|<option value="1" selected>%s</option>)' % (
+                self.ns_app_1, self.ns_app_1
+            )
+        )
+        self.assertContains(response, 'id="id_published"')
 
         self.ns_app_1.app_data.config.published_default = True
         self.ns_app_1.save()
         response = admin_instance.add_view(request)
-        self.assertContains(response, '<input checked="checked" id="id_published"')
+        response.render()
+        self.assertRegexpMatches(
+            force_text(response.content),
+            '(checked id="id_published"|id="id_published" checked|<input checked="checked" id="id_published")'
+        )
 
     def test_templatetag(self):
         article = Article.objects.create(title='news_1_app_1_config1',

.travis.yml
@@ -1,32 +1,30 @@
 language: python
 
+python: 3.6
+
 # Use container based infrastructure
 sudo: false
 
 env:
   matrix:
     - TOXENV=flake8
-    - TOXENV=py35-django110-cms34
-    - TOXENV=py35-django19-cms34
-    - TOXENV=py35-django19-cms33
-    - TOXENV=py35-django19-cms32
-    - TOXENV=py35-django18-cms34
-    - TOXENV=py35-django18-cms33
-    - TOXENV=py35-django18-cms32
-    - TOXENV=py34-django110-cms34
-    - TOXENV=py34-django19-cms34
-    - TOXENV=py34-django19-cms33
-    - TOXENV=py34-django19-cms32
-    - TOXENV=py34-django18-cms34
-    - TOXENV=py34-django18-cms33
-    - TOXENV=py34-django18-cms32
-    - TOXENV=py27-django110-cms34
-    - TOXENV=py27-django19-cms34
-    - TOXENV=py27-django19-cms33
-    - TOXENV=py27-django19-cms32
+    - TOXENV=isort
+    - TOXENV=py36-django111-cms35
+    - TOXENV=py36-django110-cms35
+    - TOXENV=py36-django19-cms35
+    - TOXENV=py36-django18-cms35
+    - TOXENV=py36-django111-cms34
+    - TOXENV=py36-django110-cms34
+    - TOXENV=py36-django19-cms34
+    - TOXENV=py36-django18-cms34
+    - TOXENV=py35-django111-cms35
+    - TOXENV=py35-django111-cms34
+    - TOXENV=py34-django111-cms35
+    - TOXENV=py34-django111-cms34
+    - TOXENV=py27-django111-cms35
+    - TOXENV=py27-django111-cms34
+    - TOXENV=py27-django18-cms35
     - TOXENV=py27-django18-cms34
-    - TOXENV=py27-django18-cms33
-    - TOXENV=py27-django18-cms32
 
 
 cache:

aldryn_apphooks_config/admin.py
@@ -23,8 +23,7 @@ class BaseAppHookConfig(AppDataModelAdmin):
     """
     readonly_fields = ('type',)
 
-    @property
-    def declared_fieldsets(self):
+    def get_fieldsets(self, request, obj):
         return [
             (None, {'fields': ('type', 'namespace')}),
             (_('Config'), {'fields': self.get_config_fields()})

aldryn_apphooks_config/models.py
@@ -53,5 +53,5 @@ class AppHookConfig(models.Model):
         """
         try:
             return getattr(self.app_data.config, item)
-        except:
+        except Exception:
             raise AttributeError('attribute %s not found' % item)

aldryn_apphooks_config/templates/aldryn_apphooks_config/admin/apphook_config_widget.html
@@ -1,0 +1,17 @@
+<script>
+    (function ($) {
+        $(document).ready(function () {
+            var sel = $('#{{ widget.attrs.id }}');
+            if (!sel.val()) {
+                sel.change(function () {
+                    $(this).apphook_reload_admin(
+                        '{{ widget.name }}', $("option:selected", $(this)).val()
+                    );
+                });
+            }
+        });
+    })(django.jQuery);
+</script>
+{% if not DJANGO_110 %}
+{% include 'django/forms/widgets/select.html' %}
+{% endif %}

aldryn_apphooks_config/tests/utils/example/admin.py
@@ -24,8 +24,7 @@ admin.site.register(Article, ArticleAdmin)
 
 class ExampleConfigAdmin(BaseAppHookConfig):
 
-    @property
-    def declared_fieldsets(self):
+    def get_fieldsets(self, request, obj):
         return [
             (None, {'fields': ('type', 'namespace', 'app_title')}),
             ('Config', {'fields': self.get_config_fields()})

aldryn_apphooks_config/tests/utils/example/cms_apps.py
@@ -9,16 +9,16 @@ from .models import ExampleConfig
 from .views import ArticleDetail, ArticleList
 
 
+@apphook_pool.register
 class ExampleApp(CMSConfigApp):
     name = 'Example'
-    urls = [
-        [
-            url(r'^$', ArticleList.as_view(), name='example_list'),
-            url(r'^(?P<slug>[\w_-]+)/$', ArticleDetail.as_view(), name='example_detail'),
-        ]
-    ]
     app_name = 'example'
     app_config = ExampleConfig
 
-
-apphook_pool.register(ExampleApp)
+    def get_urls(self, page=None, language=None, **kwargs):
+        return [
+            [
+                url(r'^$', ArticleList.as_view(), name='example_list'),
+                url(r'^(?P<slug>[\w_-]+)/$', ArticleDetail.as_view(), name='example_detail'),
+            ]
+        ]

commit 3b6402e6c4238fe573ce26014de0ae6b6b78d30b

Fix an error introduced by #54
The widget template was adapted for django 1.11+ context, not the Django 1.10- one. This provides the correct contenxt data for proper widget rendering

aldryn_apphooks_config/widgets.py
@@ -23,5 +23,7 @@ class AppHookConfigWidget(forms.Select):
         else:
             final_attrs = self.build_attrs(attrs, name=name)
             final_attrs['DJANGO_110'] = True
+            final_attrs['widget'] = final_attrs
+            final_attrs['widget']['attrs'] = attrs
             script = render_to_string(self.template_name, context=final_attrs)
             return mark_safe(script + out)

aldryn_apphooks_config/tests/test_config.py
@@ -367,6 +367,7 @@ class AppHookConfigTestCase(BaseTestCase):
         request = self.get_page_request(self.page_3, self.user)
         response = admin_instance.add_view(request)
         self.assertContains(response, '$(this).apphook_reload_admin')
+        self.assertContains(response, 'var sel = $(\'#id_section\');')
         self.assertContains(response, 'aldryn_apphooks_config')
         self.assertContains(response, '<option value="1">%s</option>' % self.ns_app_1)
         self.assertContains(response, '<option value="2">%s</option>' % self.ns_app_2)