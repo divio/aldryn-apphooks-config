b6273c1c092e94fc75d995188fbe58cbe5eb4c3c
vThaian <b.plociennik@merixstudio.com>
2019-01-02 09:27:45 +0100

    Fixed migration for Django 2.1

--- aldryn_apphooks_config/tests/utils/example/migrations/0001_initial.py
+++ aldryn_apphooks_config/tests/utils/example/migrations/0001_initial.py
@@ -37,7 +37,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=234, verbose_name='title')),
                 ('slug', models.SlugField()),
-                ('published', models.BooleanField(default=True)),
+                ('published', models.BooleanField(blank=True, default=True)),
             ],
         ),
         migrations.CreateModel(
